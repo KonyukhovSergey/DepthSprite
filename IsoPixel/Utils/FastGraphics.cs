cc3ff37d773c37518325f9067e1bbf18523f2f52
jauseg <jauseg@gmail.com>
2015-02-14 22:09:59 +0300

    added FastGraphics class

---
+++ IsoPixel/Utils/FastGraphics.cs
@@ -0,0 +1,79 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace IsoPixel
+{
+    public class FastGraphics
+    {
+        private Graphics gr;
+        private Bitmap bmp;
+
+        private BitmapData bd;
+
+        private int[] data;
+        private int stride;
+
+        public FastGraphics(int width, int height)
+        {
+            bmp = new Bitmap(width, height);
+            gr = Graphics.FromImage(bmp);
+        }
+
+        public Graphics Graphics
+        {
+            get
+            {
+                Unlock();
+                return gr;
+            }
+        }
+
+        public Bitmap Bitmap
+        {
+            get
+            {
+                Unlock();
+                return bmp;
+            }
+        }
+
+        public void Set(int color, int x, int y)
+        {
+            Lock();
+            data[x + y * stride] = color;
+        }
+
+        public int Get(int x, int y)
+        {
+            Lock();
+            return data[x + y * stride];
+        }
+
+        private void Lock()
+        {
+            if (bd == null)
+            {
+                bd = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+
+                if (data == null)
+                {
+                    stride = bd.Stride / 4;
+                    data = new int[bd.Height * stride];
+                }
+
+                Marshal.Copy(bd.Scan0, data, 0, data.Length);
+            }
+        }
+
+        private void Unlock()
+        {
+            if (bd != null)
+            {
+                Marshal.Copy(data, 0, bd.Scan0, data.Length);
+                bmp.UnlockBits(bd);
+                bd = null;
+            }
+        }
+    }
+}
---
+++ UnitTests/FastGraphicsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IsoPixel;
+using System.Drawing;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FastGraphicsTests
+    {
+        [TestMethod]
+        public void TestSetGet()
+        {
+            FastGraphics fg = new FastGraphics(3, 3);
+
+            fg.Graphics.Clear(Color.Red);
+            Assert.AreEqual(Color.Red.ToArgb(), fg.Get(0, 0));
+            Assert.AreEqual(Color.Red.ToArgb(), fg.Get(2, 2));
+
+            fg.Bitmap.SetPixel(1, 1, Color.Green);
+            Assert.AreEqual(Color.Green.ToArgb(), fg.Get(1, 1));
+
+            fg.Set(Color.Blue.ToArgb(), 2, 0);
+            Assert.AreEqual(Color.Blue.ToArgb(), fg.Bitmap.GetPixel(2, 0).ToArgb());
+        }
+    }
+}
