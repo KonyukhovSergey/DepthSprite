edf5cad49426186386e4be6e7a1ce6e807c72140
jauseg <jauseg@gmail.com>
2015-02-20 23:21:15 +0300

    added CommandProcessor

---
+++ IsoPixel/Commands/CommandsProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsoPixel
+{
+    public class CommandsProcessor
+    {
+        private List<CommandBase> commands = new List<CommandBase>();
+        private int currentCommandIndex = 0;
+        private int historySize = 256;
+
+        public void Execute(CommandBase command)
+        {
+            if (command.Execute())
+            {
+                commands.RemoveRange(currentCommandIndex, commands.Count - currentCommandIndex);
+                commands.Add(command);
+                currentCommandIndex++;
+
+                if (commands.Count > historySize)
+                {
+                    commands.RemoveAt(0);
+                    currentCommandIndex--;
+                }
+            }
+        }
+
+        public void Undo()
+        {
+            if (currentCommandIndex > 0)
+            {
+                currentCommandIndex--;
+                commands[currentCommandIndex].Undo();
+            }
+        }
+
+        public void Redo()
+        {
+            if (currentCommandIndex < commands.Count)
+            {
+                commands[currentCommandIndex].Execute();
+                currentCommandIndex++;
+            }
+        }
+    }
+}
